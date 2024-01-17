766519640732859ffb9cf51845f78900c30b5f56
vijayghore2024 <vijayghore2024@gmail.com>
2024-01-17 09:00:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from "./components/Calculator";
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+  return (
+    <div className="App">
+        <NavigationBar />
+        <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <div className='container'>
+      <h1 className='text-center my-3'>myCalc</h1>
+
+      <div>
+        <div>
+            input tag
+        </div>
+        <div>
+            buttons
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Calculator
